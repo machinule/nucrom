07ee7fc819532857f74591d1314d90d127fd0467
machinule <machinule@users.noreply.github.com>
2016-04-02 16:00:45 -0700

    Initial SDL window + GL Context

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import "github.com/machinule/nucrom/frontend/sdl2"
+
 func main() {
+	fe, err := sdl2.New()
+	if err != nil {
+		panic(err)
+	}
+	if err := fe.Run(); err != nil {
+		panic(err)
+	}
 }
