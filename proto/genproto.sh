dd52dfe5f3feaba26f37ab981e27b0aad8e9f9b1
David Mihai <david.gmihai@gmail.com>
2016-04-04 19:21:55 -0700

    Added more protos, segregated into systems

--- proto/genproto.sh
+++ proto/genproto.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -rf $DIR/gen/*
-protoc --go_out=$DIR/gen --proto_path=$DIR/src $DIR/src/*.proto $DIR/src/mechanics/*.proto
+protoc --go_out=$DIR/gen --proto_path=$DIR/src $DIR/src/*.proto $DIR/src/mechanics/*.proto $DIR/src/systems/*.proto $DIR/src/common/*.proto
+echo "Proto compiled"
