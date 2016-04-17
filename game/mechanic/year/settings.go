4fc55ed86a8ea4ef1996a53378eb700bca3829a2
machinule <machinule@users.noreply.github.com>
2016-04-17 12:44:37 -0700

    also fix settings
    missed it on the last commit.

--- game/mechanic/year/settings.go
+++ game/mechanic/year/settings.go
@@ -5,7 +5,8 @@ import (
 	pb "github.com/machinule/nucrom/proto/gen"
 )
 
-type settings struct {
+// Settings contains parameters that do not change over the course of a game.
+type Settings struct {
 	init int32 // Starting year.
 	incr int32 // Number of years to increment per turn.
 }
@@ -15,19 +16,21 @@ func validate(settingsProto *pb.GameSettings) error {
 	return nil
 }
 
-func NewSettings(settingsProto *pb.GameSettings) (*settings, error) {
+// NewSettings creates a new settings struct from the GameSettings message.
+func NewSettings(settingsProto *pb.GameSettings) (*Settings, error) {
 	if err := validate(settingsProto); err != nil {
 		return nil, fmt.Errorf("validating settings proto: %e", err)
 	}
-	return &settings{
+	return &Settings{
 		init: settingsProto.GetYearSysSettings().GetInitYear(),
 		incr: 1,
 	}, nil
 }
 
-func (s *settings) InitState() (*state, error) {
-	return &state{
-		s:    s,
-		year: s.init,
+// InitState creates the initial state for the year mechanic.
+func (s *Settings) InitState() (*State, error) {
+	return &State{
+		settings: s,
+		year:     s.init,
 	}, nil
 }
