5d48ebac2b3c34d574af9af24a8f5d93f7020cca
David Mihai <david.gmihai@gmail.com>
2016-05-08 15:24:48 -0700

    (PROVINCES) Conflicts (State only)

--- game/mechanic/province/state_test.go
+++ game/mechanic/province/state_test.go
@@ -90,11 +90,280 @@ func TestNewState(t *testing.T) {
 	}
 }
 
+type NewConflictCase struct {
+	proto       *pb.GameState
+	prev        *State
+	id          pb.ProvinceId
+	name        string
+	att         pb.ProvinceId
+	def         pb.ProvinceId
+	goal        int32
+	length      int32
+	base_chance int32
+	locations   pb.ProvinceId
+	att_sup     pb.Player
+	def_sup     pb.Player
+	att_prog    int32
+	def_prog    int32
+	err         bool
+}
+
+func TestNewConflictState(t *testing.T) {
+	cases := []NewConflictCase{
+		{
+			proto: &pb.GameState{
+				ProvincesState: &pb.ProvincesState{
+					ProvinceStates: []*pb.ProvinceState{},
+					Conflicts: &pb.ConflictsState{
+						Active: []*pb.Conflict{
+							&pb.Conflict{
+								Name: "American Revolution",
+								Type: pb.ConflictType_COLONIAL_WAR,
+								Goal: 5,
+								Attackers: &pb.Faction{
+									Ids: []pb.ProvinceId{ // TODO: Dissidents
+										pb.ProvinceId_P_USA,
+									},
+									Supporter: pb.Player_USSR, // Basically the French
+									Progress:  3,
+								},
+								Defenders: &pb.Faction{
+									Ids: []pb.ProvinceId{
+										pb.ProvinceId_GREAT_BRITAIN,
+									},
+									Supporter: pb.Player_NEITHER,
+									Progress:  2,
+								},
+								Length:     6,
+								BaseChance: 150000,
+							},
+						},
+						Dormant:  []*pb.Conflict{},
+						Possible: []*pb.Conflict{},
+					},
+				},
+			},
+			prev: &State{
+				settings: &Settings{},
+				Conflicts: map[pb.ProvinceId]*Conflict{
+					pb.ProvinceId_P_USA: &Conflict{
+						name:          "American Revolution",
+						conflict_type: pb.ConflictType_COLONIAL_WAR,
+						goal:          6,
+						attackers: Faction{
+							members: []pb.ProvinceId{
+								pb.ProvinceId_P_USA,
+							},
+							supporter: pb.Player_USSR,
+							progress:  4,
+						},
+						defenders: Faction{
+							members: []pb.ProvinceId{
+								pb.ProvinceId_GREAT_BRITAIN,
+							},
+							supporter: pb.Player_NEITHER,
+							progress:  3,
+						},
+						length:      7,
+						base_chance: 150000,
+					},
+				},
+			},
+			id:          pb.ProvinceId_P_USA,
+			name:        "American Revolution",
+			att:         pb.ProvinceId_P_USA,
+			def:         pb.ProvinceId_GREAT_BRITAIN,
+			goal:        6,
+			length:      7,
+			base_chance: 150000,
+			locations:   pb.ProvinceId_P_USA,
+			att_sup:     pb.Player_USSR,
+			def_sup:     pb.Player_NEITHER,
+			att_prog:    4,
+			def_prog:    3,
+			err:         false,
+		},
+		{
+			proto: &pb.GameState{
+				ProvincesState: &pb.ProvincesState{
+					ProvinceStates: []*pb.ProvinceState{},
+					Conflicts:      &pb.ConflictsState{},
+				},
+			},
+			prev: nil,
+			err:  true,
+		},
+	}
+	for _, tc := range cases {
+		s, err := NewState(tc.proto, tc.prev)
+		if got, want := err != nil, tc.err; got != want {
+			msg := map[bool]string{
+				true:  "error",
+				false: "no error",
+			}
+			t.Errorf("err: got %s, want %s", msg[got], msg[want])
+			continue
+		}
+		if tc.err {
+			continue
+		}
+		c := s.Conflict(tc.id)
+		if got, want := c.Name(), tc.name; got != want {
+			t.Errorf("name: got %d, want %d", got, want)
+		}
+		if got, want := c.Goal(), tc.goal; got != want {
+			t.Errorf("goal: got %d, want %d", got, want)
+		}
+		if got, want := c.Defenders()[0], tc.def; got != want {
+			t.Errorf("defenders: got %d, want %d", got, want)
+		}
+		if got, want := c.Attackers()[0], tc.att; got != want {
+			t.Errorf("attackers: got %d, want %d", got, want)
+		}
+		if got, want := c.Attackers()[0], tc.att; got != want {
+			t.Errorf("attackers: got %d, want %d", got, want)
+		}
+		if got, want := c.Length(), tc.length; got != want {
+			t.Errorf("length: got %d, want %d", got, want)
+		}
+		if got, want := c.BaseChance(), tc.base_chance; got != want {
+			t.Errorf("base_chance: got %d, want %d", got, want)
+		}
+		if got, want := c.Att_Supporter(), tc.att_sup; got != want {
+			t.Errorf("att sup: got %d, want %d", got, want)
+		}
+		if got, want := c.Att_Progress(), tc.att_prog; got != want {
+			t.Errorf("att prog: got %d, want %d", got, want)
+		}
+		if got, want := c.Def_Supporter(), tc.def_sup; got != want {
+			t.Errorf("def sup: got %d, want %d", got, want)
+		}
+		if got, want := c.Def_Progress(), tc.def_prog; got != want {
+			t.Errorf("def prog: got %d, want %d", got, want)
+		}
+		if got, want := s.settings, tc.prev.settings; got != want {
+			t.Errorf("settings: got %d, want %d", got, want)
+		}
+	}
+}
+
 type MarshalCase struct {
 	s     State
 	proto *pb.GameState
 	want  *pb.GameState
 	err   bool
+}
+
+func TestConflictMarshal(t *testing.T) {
+	cases := []MarshalCase{
+		{
+			s: State{
+				Conflicts: map[pb.ProvinceId]*Conflict{
+					pb.ProvinceId_P_USA: &Conflict{
+						name:          "American Revolution",
+						conflict_type: pb.ConflictType_COLONIAL_WAR,
+						goal:          6,
+						attackers: Faction{
+							members: []pb.ProvinceId{
+								pb.ProvinceId_P_USA,
+							},
+							supporter: pb.Player_USSR,
+							progress:  4,
+						},
+						defenders: Faction{
+							members: []pb.ProvinceId{
+								pb.ProvinceId_GREAT_BRITAIN,
+							},
+							supporter: pb.Player_NEITHER,
+							progress:  3,
+						},
+						length:      7,
+						base_chance: 150000,
+					},
+				},
+			},
+			proto: &pb.GameState{},
+			want: &pb.GameState{
+				ProvincesState: &pb.ProvincesState{
+					ProvinceStates: []*pb.ProvinceState{},
+					Conflicts: &pb.ConflictsState{
+						Active: []*pb.Conflict{
+							&pb.Conflict{
+								Name: "American Revolution",
+								Type: pb.ConflictType_COLONIAL_WAR,
+								Goal: 6,
+								Attackers: &pb.Faction{
+									Ids: []pb.ProvinceId{ // TODO: Dissidents
+										pb.ProvinceId_P_USA,
+									},
+									Supporter: pb.Player_USSR, // Basically the French
+									Progress:  4,
+								},
+								Defenders: &pb.Faction{
+									Ids: []pb.ProvinceId{
+										pb.ProvinceId_GREAT_BRITAIN,
+									},
+									Supporter: pb.Player_NEITHER,
+									Progress:  3,
+								},
+								Length:     7,
+								BaseChance: 150000,
+							},
+						},
+						Dormant:  []*pb.Conflict{},
+						Possible: []*pb.Conflict{},
+					},
+				},
+			},
+			err: false,
+		},
+		{
+			s: State{
+				Conflicts: map[pb.ProvinceId]*Conflict{
+					pb.ProvinceId_P_USA: &Conflict{
+						name:          "American Revolution",
+						conflict_type: pb.ConflictType_COLONIAL_WAR,
+						goal:          6,
+						attackers: Faction{
+							members: []pb.ProvinceId{
+								pb.ProvinceId_P_USA,
+							},
+							supporter: pb.Player_USSR,
+							progress:  4,
+						},
+						defenders: Faction{
+							members: []pb.ProvinceId{
+								pb.ProvinceId_GREAT_BRITAIN,
+							},
+							supporter: pb.Player_NEITHER,
+							progress:  3,
+						},
+						length:      7,
+						base_chance: 150000,
+					},
+				},
+			},
+			proto: nil,
+			err:   true,
+		},
+	}
+	for _, tc := range cases {
+		err := tc.s.Marshal(tc.proto)
+		if got, want := err != nil, tc.err; got != want {
+			msg := map[bool]string{
+				true:  "error",
+				false: "no error",
+			}
+			t.Errorf("err: got %s, want %s", msg[got], msg[want])
+			continue
+		}
+		if tc.err {
+			continue
+		}
+		if got, want := tc.proto, tc.want; !proto.Equal(got, want) {
+			t.Errorf("GameState: got %v, want %v", got, want)
+		}
+	}
 }
 
 func TestMarshal(t *testing.T) {
@@ -123,6 +392,7 @@ func TestMarshal(t *testing.T) {
 							Leader: "David Mihai",
 						},
 					},
+					Conflicts: &pb.ConflictsState{},
 				},
 			},
 			err: false,
